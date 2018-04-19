664b81d27f5198503d4cb8f9eb4823380729e275
Grabie Eliott <eliott.grabie@gmail.com>
2018-04-19 14:15:49 +0200

    Debut

---
+++ piscineIOT/piscineIOT/piscineIOT.ino
@@ -0,0 +1,47 @@
+/*
+ Name:		piscineIOT.ino
+ Created:	19/04/2018 14:12:28
+ Author:	Eliott
+*/
+/*TODO
+capteur :
+-temp ds1820 lib one wire
+-ultrason ??? lib ??
+-bac tampon
+-bidon ph+
+-bidon chlore
+-sondePH ??? lib phMetre.h
+
+control :
+-pompe chlore
+-pompe PH+
+-relay piscine ???
+-relay robot
+-valve robot ???
+
+notion :
+-horloge
+-cycle loop 5/10/30/60min ?
+-event ???
+
+idee:
+-pour stat: entree valeur chlore journaliere sur le tel blynk save auto in base
+
+
+
+
+
+*/
+void setup()
+{
+
+	/* add setup code here */
+
+}
+
+void loop()
+{
+
+	/* add main program code here */
+
+}
